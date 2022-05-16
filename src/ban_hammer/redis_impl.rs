19fb03f31676b93a102e020bdf89746767b90e10
hehegrrrhehegrr <ilwhmcwo@tuta.io>
2022-05-16 18:07:16 +0300

    Move 'store' method into 'ban_hammer'

--- src/ban_hammer/redis_impl.rs
+++ src/ban_hammer/redis_impl.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
 
 use crate::ban_hammer::BanHammer;
@@ -9,7 +11,8 @@ use crate::redis::Service;
 #[async_trait]
 impl BanHammer for Service {
     async fn ban(&self, be: BanEntity) -> Result<(), BanError> {
-        self.store(
+        store(
+            self,
             be.target.clone(),
             be.analyzer.clone(),
             be.reason.clone(),
@@ -18,4 +21,46 @@ impl BanHammer for Service {
         .await
         .map_err(|e| errors::BanError::Error(e))
     }
+}
+
+async fn store(
+    redis: &Service,
+    key: String,
+    anl: String,
+    reason: String,
+    ttl: u32,
+) -> Result<(), errors::Redis> {
+    tokio::time::timeout(redis.timeout, _store(redis, key, anl, reason, ttl))
+        .await
+        .map_err(|_| errors::Redis::Timeout)?
+}
+
+async fn _store(
+    redis: &Service,
+    key: String,
+    anl: String,
+    reason: String,
+    ttl: u32,
+) -> Result<(), errors::Redis> {
+    let pool = redis.pool.clone();
+
+    let mut conn = match pool.get().await {
+        Ok(c) => c,
+        Err(e) => {
+            return Err(errors::Redis::GetConnection(Arc::new(e)));
+        }
+    };
+
+    #[rustfmt::skip]
+    redis::pipe()
+        .atomic()
+        .hset(&key, anl, reason).ignore()
+        .expire(&key, ttl as usize).arg("NX").ignore()
+        .expire(&key, ttl as usize).arg("GT").ignore()
+        .query_async(&mut *conn).await
+        .map_err(|re| errors::Redis::Pipeline(
+            Arc::from(re),
+            vec!["HSET".into(), "EXPIRE NX".into(), "EXPIRE GT".into()]))?;
+
+    Ok(())
 }
--- src/redis/service.rs
+++ src/redis/service.rs
@@ -1,4 +1,3 @@
-use std::sync::Arc;
 use std::time;
 
 use bb8::Pool;
@@ -19,47 +18,5 @@ impl Service {
     ) -> Result<Self, errors::Redis> {
         let timeout = time::Duration::from_secs(timeout_secs);
         Ok(Service { pool, timeout })
-    }
-
-    pub async fn store(
-        self: &Self,
-        key: String,
-        anl: String,
-        reason: String,
-        ttl: u32,
-    ) -> Result<(), errors::Redis> {
-        tokio::time::timeout(self.timeout, self._store(key, anl, reason, ttl))
-            .await
-            .map_err(|_| errors::Redis::Timeout)?
-    }
-
-    async fn _store(
-        self: &Self,
-        key: String,
-        anl: String,
-        reason: String,
-        ttl: u32,
-    ) -> Result<(), errors::Redis> {
-        let pool = self.pool.clone();
-
-        let mut conn = match pool.get().await {
-            Ok(c) => c,
-            Err(e) => {
-                return Err(errors::Redis::GetConnection(Arc::new(e)));
-            }
-        };
-
-        #[rustfmt::skip]
-        redis::pipe()
-            .atomic()
-            .hset(&key, anl, reason).ignore()
-            .expire(&key, ttl as usize).arg("NX").ignore()
-            .expire(&key, ttl as usize).arg("GT").ignore()
-            .query_async(&mut *conn).await
-            .map_err(|re| errors::Redis::Pipeline(
-                Arc::from(re),
-                vec!["HSET".into(), "EXPIRE NX".into(), "EXPIRE GT".into()]))?;
-
-        Ok(())
     }
 }
