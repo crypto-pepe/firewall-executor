7b10a28cf0dd83a8dde52478f8752c8f13c78feb
hehegrrrhehegrr <ilwhmcwo@tuta.io>
2022-05-17 17:41:35 +0300

    Use struct instead of vector

--- src/model.rs
+++ src/model.rs
@@ -1,39 +1,28 @@
-use std::collections::BTreeMap;
 use std::fmt::{Debug, Display, Formatter};
 
 use serde::{Deserialize, Serialize};
 
 use crate::http_error::BanTargetConversionError;
 
-const TARGET_TYPE_ORDER: &[TargetType] = &[TargetType::IP, TargetType::UserAgent];
-
 #[derive(Debug, Serialize, Deserialize)]
-pub enum TargetType {
-    #[serde(rename = "ip")]
-    IP,
-    #[serde(rename = "user-agent")]
-    UserAgent,
-}
-
-impl Display for TargetType {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str(match self {
-            TargetType::IP => "ip",
-            TargetType::UserAgent => "user-agent",
-        })
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub struct BanTarget {
-    #[serde(rename = "type")]
-    pub target_type: TargetType,
-    pub value: String,
+    pub ip: String,
+    pub user_agent: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct BanTargetRequest {
-    pub target: Vec<BanTarget>,
+impl Display for BanTarget {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.user_agent.is_none() {
+            f.write_str(&*self.ip)
+        } else {
+            f.write_str(&*format!(
+                "{}_{}",
+                &*self.ip,
+                self.user_agent.as_ref().unwrap()
+            ))
+        }
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -43,33 +32,6 @@ pub struct BanEntity {
     pub target: String,
     pub reason: String,
     pub ttl: u32,
-}
-
-pub fn target_to_key(bt: &Vec<BanTarget>) -> Result<String, BanTargetConversionError> {
-    let mut bt_value = BTreeMap::new();
-
-    for t in bt {
-        if bt_value.get(&t.target_type.to_string()).is_none() {
-            bt_value.insert(t.target_type.to_string(), t.value.to_string());
-        } else {
-            return Err(BanTargetConversionError::InvalidTypeCount);
-        }
-    }
-
-    let target = TARGET_TYPE_ORDER
-        .iter()
-        .fold(String::new(), |res: String, t| {
-            if let Some(v) = bt_value.get(&*t.to_string()) {
-                format!("{}{}{}", res, t, v)
-            } else {
-                res
-            }
-        });
-
-    if target.is_empty() {
-        return Err(BanTargetConversionError::InvalidTypeCount);
-    }
-    Ok(target)
 }
 
 impl BanEntity {
@@ -84,7 +46,7 @@ impl BanEntity {
             .ttl
             .ok_or_else(|| BanTargetConversionError::FieldRequired("ttl".to_string()))?;
 
-        let target = target_to_key(&target)?;
+        let target = target.to_string();
         Ok(BanEntity {
             analyzer,
             ttl,
@@ -97,7 +59,7 @@ impl BanEntity {
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub struct BanRequest {
-    pub target: Option<Vec<BanTarget>>,
+    pub target: Option<BanTarget>,
     pub reason: Option<String>,
     pub ttl: Option<u32>,
 }
@@ -110,96 +72,36 @@ impl Display for BanRequest {
 
 #[cfg(test)]
 mod tests {
-    use crate::http_error::BanTargetConversionError;
-    use crate::model::{target_to_key, BanTarget, TargetType};
+    use crate::model::BanTarget;
 
     struct TestCase {
-        pub input: Vec<BanTarget>,
-        pub want: Result<String, BanTargetConversionError>,
+        pub input: BanTarget,
+        pub want: String,
     }
 
     #[test]
-    fn target_to_key_ip_only() {
+    fn target_to_key_ip() {
         let tc = TestCase {
-            input: vec![BanTarget {
-                target_type: TargetType::IP,
-                value: "1.1.1.1".into(),
-            }],
-            want: Ok("ip1.1.1.1".into()),
+            input: BanTarget {
+                ip: "1.1.1.1".into(),
+                user_agent: None,
+            },
+            want: "1.1.1.1".into(),
         };
 
-        assert_eq!(target_to_key(&tc.input), tc.want);
+        assert_eq!(tc.input.to_string(), tc.want);
     }
 
     #[test]
     fn target_to_key_ip_and_user_agent() {
         let tc = TestCase {
-            input: vec![
-                BanTarget {
-                    target_type: TargetType::IP,
-                    value: "1.1.1.1".into(),
-                },
-                BanTarget {
-                    target_type: TargetType::UserAgent,
-                    value: "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0".into(),
-                },
-            ],
-            want: Ok("ip1.1.1.1user-agentMozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0".into()),
+            input: BanTarget {
+                ip: "1.1.1.1".into(),
+                user_agent: Some("abc".into()),
+            },
+            want: "1.1.1.1_abc".into(),
         };
 
-        assert_eq!(target_to_key(&tc.input), tc.want);
-    }
-
-    #[test]
-    fn target_to_key_ip_and_2_user_agent() {
-        let tc = TestCase {
-            input: vec![
-                BanTarget {
-                    target_type: TargetType::IP,
-                    value: "1.1.1.1".into(),
-                },
-                BanTarget {
-                    target_type: TargetType::UserAgent,
-                    value: "Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"
-                        .into(),
-                },
-                BanTarget {
-                    target_type: TargetType::UserAgent,
-                    value: "Some other user-agent".into(),
-                },
-            ],
-            want: Err(BanTargetConversionError::InvalidTypeCount),
-        };
-
-        assert_eq!(target_to_key(&tc.input), tc.want);
-    }
-
-    #[test]
-    fn target_to_key_2_ip() {
-        let tc = TestCase {
-            input: vec![
-                BanTarget {
-                    target_type: TargetType::IP,
-                    value: "1.1.1.1".into(),
-                },
-                BanTarget {
-                    target_type: TargetType::IP,
-                    value: "2.2.2.2".into(),
-                },
-            ],
-            want: Err(BanTargetConversionError::InvalidTypeCount),
-        };
-
-        assert_eq!(target_to_key(&tc.input), tc.want);
-    }
-
-    #[test]
-    fn target_to_key_empty() {
-        let tc = TestCase {
-            input: vec![],
-            want: Err(BanTargetConversionError::InvalidTypeCount),
-        };
-
-        assert_eq!(target_to_key(&tc.input), tc.want);
+        assert_eq!(tc.input.to_string(), tc.want);
     }
 }
